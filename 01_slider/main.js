c55ec05e808d5a0d191c1ef1d2d6c02db7b1ef15
Alexey Gayduk <le5h@ya.ru>
2018-06-27 12:47:28 +0300

    Finished beta

--- 01_slider/main.js
+++ 01_slider/main.js
@@ -3,63 +3,80 @@ $(document).ready(function(){
     var $debug = $('#debug');
 
     Slider = {
-        $slider: null,
-        $items: null,
-        itemsCount: 0,
-        $current: null,
-        currentId: 1,
-        threshold: 10,
-        touch: {
-            startPoint: 0,
-            currPoint: 0,
-            endPoint: 0
-        },
+        $slider: null, width: 0,
+        $items: null, itemsCount: 0, /* TODO: <3 items */
+        currentId: 0, previousId: 0, nextId: 0,
+        $current: null, $previous: null, $next: null,
+        threshold: 100, /* px, to pass slide effect */
+        touch: { startPoint: 0, currPoint: 0, endPoint: 0 },
         setCurrent: function(){
-            this.$items.removeClass('current');
+            this.$items.removeClass('current previous next');
             this.$current = this.$items.filter(':nth-child(' + this.currentId + ')');
             this.$current.addClass('current');
+            this.$previous = this.$items.filter(':nth-child(' + this.previousId + ')');
+            this.$previous.addClass('previous');
+            this.$next = this.$items.filter(':nth-child(' + this.nextId + ')');
+            this.$next.addClass('next');
         },
         moveCurrent: function(amount) {
-            // this.$current.css('left', amount);
             this.$current.css('transform', 'translateX(' + amount + 'px)');
+            this.$previous.css('transform', 'translateX(' + (amount - this.width) + 'px)');
+            this.$next.css('transform', 'translateX(' + (amount + this.width) + 'px)');
         },
-        slideCurrent: function(value) {
+        freeCurrent: function(){
+            this.$current.css('transform', '');
+        },
+        slideThreshold: function(value) {
             if(Math.abs(value) > this.threshold){
-                if(value < 0) { this.currentId--; }
-                if(value > 0) { this.currentId++; }
-                if(this.currentId < 1) { this.currentId = this.itemsCount; }
-                if(this.currentId > this.itemsCount) { this.currentId = 1; }
-                this.setCurrent();
+                this.slide(value);
             }
+        },
+        slide: function(value){
+            // change current
+            if(value > 0) { this.currentId--; }
+            if(value < 0) { this.currentId++; }
+            if(this.currentId < 1) { this.currentId = this.itemsCount; }
+            if(this.currentId > this.itemsCount) { this.currentId = 1; }
+            // calc prev next
+            this.previousId = this.currentId - 1;
+            if(this.previousId < 1) { this.previousId = this.itemsCount; }
+            if(this.previousId > this.itemsCount) { this.previousId = 1; }
+            this.nextId = this.currentId + 1;
+            if(this.nextId < 1) { this.nextId = this.itemsCount; }
+            if(this.nextId > this.itemsCount) { this.nextId = 1; }
+            this.setCurrent();
         },
         init: function(selector){
 
             this.$slider = $(selector);
+            this.width = this.$slider.width();
             this.$items = this.$slider.children(selector + '-item');
             this.itemsCount = this.$items.length;
-
-            this.setCurrent();
+            this.currentId = 1;
+            this.slide(0);
 
             this.$slider.on('touchstart',function(ev){
                 Slider.touch.startPoint = Number(ev.changedTouches[0].clientX);
-                $debug.append('start: ' + Slider.touch.startPoint);
+                Slider.$slider.addClass('moving');
                 ev.preventDefault();
             });
         
             this.$slider.on('touchmove',function(ev){
                 Slider.touch.currPoint = Number(ev.changedTouches[0].clientX);
-                $debug.append(',' + Slider.touch.currPoint);
                 Slider.moveCurrent(Slider.touch.currPoint - Slider.touch.startPoint);
                 ev.preventDefault();
             });
         
             this.$slider.on('touchend',function(ev){
                 Slider.touch.endPoint = Number(ev.changedTouches[0].clientX);
-                $debug.append(' end: ' + Slider.touch.endPoint + ';<br>');
-                Slider.slideCurrent(Slider.touch.endPoint - Slider.touch.startPoint);
+                Slider.$slider.removeClass('moving');
+                Slider.freeCurrent();
+                Slider.slideThreshold(Slider.touch.endPoint - Slider.touch.startPoint);
                 Slider.moveCurrent(0);
                 ev.preventDefault();
             });
+
+            // TODO: onresize
 
         }
     }
